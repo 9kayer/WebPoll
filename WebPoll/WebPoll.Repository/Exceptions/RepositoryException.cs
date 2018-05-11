a971f0b97fa98f580a831e3998f27a61e1c20fff
9kayer <fabio.9kayer@gmail.com>
2018-05-11 10:57:28 +0100

    Exceptions created

---
+++ WebPoll/WebPoll.Repository/Exceptions/RepositoryException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebPoll.Model.Models;
+
+namespace WebPoll.Repository.Exceptions
+{
+    public abstract class RepositoryException : Exception
+    {
+        public RepositoryException() : base()
+        {
+        }
+
+        public RepositoryException(string message) : base(message)
+        {
+        }
+
+        public RepositoryException(string message, Exception ex) : base(message, ex)
+        {
+        }
+
+    }
+}
