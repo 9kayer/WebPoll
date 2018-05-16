43779af36f4f5116e566de33c627e22b23dc94d6
9kayer <fabio.9kayer@gmail.com>
2018-05-16 15:11:17 +0100

    Added GenreController and MusicController

---
+++ WebPoll/WebPoll.Web/Controllers/GenreController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebPoll.Model.Models;
+using WebPoll.Web.Services;
+using AutoMapper;
+using WebPoll.Web.DTO;
+using System.Collections;
+
+namespace WebPoll.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Genre")]
+    public class GenreController : Controller
+    {
+        private GenreService _service;
+        private IMapper _mapper;
+
+        public GenreController(GenreService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        // GET: api/Genre
+        [HttpGet]
+        public ICollection<GenreResponse> Get()
+        {
+            return _mapper.Map<ICollection<Genre>, ICollection<GenreResponse>>(_service.GetAll());
+        }
+
+        // GET: api/Genre/5
+        [HttpGet("{id}")]
+        public GenreResponse Get(int id)
+        {
+            return _mapper.Map<Genre,GenreResponse>(_service.GetById(id));
+        }
+        
+        // POST: api/Genre
+        [HttpPost]
+        public void Post([FromBody]GenreRequest genreRequest)
+        {
+            _service.Create( _mapper.Map<GenreRequest, Genre>(genreRequest) );
+        }
+        
+        // PUT: api/Genre/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]GenreRequest genreRequest)
+        {
+            _service.Update(_mapper.Map<GenreRequest, Genre>(genreRequest));
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
+    }
+}
---
+++ WebPoll/WebPoll.Web/Controllers/MusicController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebPoll.Web.DTO;
+using WebPoll.Web.Services;
+using AutoMapper;
+using WebPoll.Model.Models;
+
+namespace WebPoll.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Music")]
+    public class MusicController : Controller
+    {
+        private MusicService _service;
+        private IMapper _mapper;
+
+        public MusicController(MusicService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        // GET: api/Music
+        [HttpGet]
+        public ICollection<MusicResponse> Get()
+        {
+            return _mapper.Map<ICollection<Music>, ICollection<MusicResponse>>(_service.GetAll());
+        }
+
+        // GET: api/Music/{id}
+        [HttpGet("{id}")]
+        public MusicResponse Get(int id)
+        {
+            return _mapper.Map<Music, MusicResponse>(_service.GetById(id));
+        }
+        
+        // POST: api/Music
+        [HttpPost]
+        public void Post([FromBody]MusicRequest musicRequest)
+        {
+            _service.Create(_mapper.Map<MusicRequest, Music>(musicRequest));
+        }
+
+        // PUT: api/Music/{id}
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]MusicRequest musicRequest)
+        {
+            _service.Update(_mapper.Map<MusicRequest, Music>(musicRequest));
+        }
+
+        // DELETE: api/Music/{id}
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
+    }
+}
